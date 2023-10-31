d012c5e556f46decf3469195ad41d9c2ecf6abfd
mobin khayati <94926641+mobinkhayati@users.noreply.github.com>
2023-10-31 15:08:47 +0330

    Update Gaussian Membership Function.py

--- Membership Function/Gaussian Membership Function.py
+++ Membership Function/Gaussian Membership Function.py
@@ -1,1 +1,21 @@
+import numpy as np
+import skfuzzy as fuzz
+import matplotlib.pyplot as plt
 
+# تعریف محدوده ورودی
+x = np.arange(-5, 5, 0.1)
+
+# تعریف پارامترهای تابع عضویت گوسی
+mean = 0.0  # میانگین
+sigma = 1.0  # انحراف معیار
+
+# تعریف تابع عضویت گوسی
+membership = fuzz.gaussmf(x, mean, sigma)
+
+# نمایش تابع عضویت گوسی
+plt.figure()
+plt.plot(x, membership, 'b', linewidth=1.5)
+plt.title('Gaussian Membership Function')
+plt.ylabel('Membership')
+plt.xlabel('input variable (x)')
+plt.show()
